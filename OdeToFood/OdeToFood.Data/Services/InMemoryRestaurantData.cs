2c03835235e35d93005b040e478e41c59e4ad45f
Bruno de Renzis <brunoderenzisrubinetti26@gmail.com>
2021-05-11 21:15:49 -0300

    Primer vista MVC

---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data.Models;
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id = 1, Name="Ugi's",Cuisine = CuisineType.Italian},
+                new Restaurant{Id = 2, Name="McDonald",Cuisine = CuisineType.None},
+                new Restaurant{Id = 2, Name="Apu's",Cuisine = CuisineType.Indian}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r=> r.Name);
+        }
+    }
+}
